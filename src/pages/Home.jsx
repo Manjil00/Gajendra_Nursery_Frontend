cf807b5992096e27b36f6daffb73d13b465a090f
ACER <210220@softwarica.edu.np>
2024-07-19 22:31:15 +0545

    responsive till sec3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,194 +1,151 @@
-import React from 'react';
-import "./Home.css";
-import "./Product.jsx";
+import React, { useState } from "react";
+import CountUp from 'react-countup';
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import './index.css';
 
-//Icons
-import { FaShoppingCart } from "react-icons/fa"; //CART
-import { FcServices, FcShipped, FcShop } from "react-icons/fc";
-import { RiShoppingBag4Fill } from "react-icons/ri"; //ShoppingBag
+const Home = () => {
 
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 3
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 2
+        }
+    };
 
-//SLIDER CARASOUEL
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+    const [isNavOpen, setIsNavOpen] = useState(false);
+    const toggleNavMenu = () => setIsNavOpen(!isNavOpen);
 
+    return (
+        <div className="App">
+            <nav className="flex justify-between px-6 py-3">
+                <div className="flex justify-center items-center">
+                    <img className="w-10 h-10 mr-3" src="./images/GN_logo.png" alt="Logo" />
+                    <h1 className='text-black ml-6  text-sm md:text-4xl font-LogoFont
+            cursor-pointer flex justify-center items-center'>Gajendra Nursery</h1>
+                </div>
+                <div className={`bg-white absolute min-h-[30vh] left-0 top-[9%] w-full p-5 flex flex-col md:static md:flex md:min-h-fit md:w-auto gap-8 text-sm ${isNavOpen ? 'block' : 'hidden'}`}>
+                    <ul className=" cursor-pointer flex flex-col gap-4 md:flex-row  md:gap-8 md:justify-evenly md:text-xl font-writingFont">
+                        <li className="hover:text-green-600">Home</li>
+                        <li className="hover:text-green-600">Products</li>
+                        <li className="hover:text-green-600">Decorations</li>
+                        <li className="hover:text-green-600">Pots/Ceramics</li>
+                        <li className="hover:text-green-600">Blogs</li>
+                        <li className="hover:text-green-600">About us</li>
+                    </ul>
+                </div>
+                <div>
+                    <button onClick={toggleNavMenu} className="md:hidden">
+                        {
+                        isNavOpen ? <RxCross1 className="text-2xl text-black" /> :
+                        <RxHamburgerMenu className={`text-2xl text-black  ${isNavOpen ? 'hidden' : 'block'}`} />
+                        }
+                    </button>
+                </div>
+            </nav>
+            
 
+            <div className="bodymain ">
+            <div className=" border-2 flex  w-[100%] h-[300px] mt-[5px] md:h-[500px] object-contain">
 
+<div className="relative w-[70%] h-[300px] md:h-[500px] z-[-1]">
+    <img src="./images/pic1.jpg" className="object-cover w-full h-full " alt="pic1"/>
+    <p className="absolute left-2 top-[12%]  text-sm font-section1Font object-hidden  md:top-[15%] md:text-3xl md:left-10 font-LogoFont ">YOUR . ONE . STOP . SHOP .
+      <br/>FOR . ALL .
+    <br/>
+      <span className="text-green-700 text-xl md:text-6xl"> BOTANIST
 
-function Home() {
-
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
-
-return (
-    <div className='home'>
-  <div className='navbar'>
-    <nav>
-      <img className='logoimage' src='../images/GN_logo.png' alt="logo"/>
-      <h2 className="logo">Gajendra <span>Nursery</span></h2>
-      <ul>
-        <li> <a href="Product">Product</a></li>
-        <li> <a href="Decorations">Decorations</a></li>
-        <li> <a href="Blogs">Blogs</a></li>
-        <li> <a href="AboutUs">About Us</a></li>
-      </ul>
-      <button type='button'> Cart   <FaShoppingCart /> </button>
-    </nav>
+        </span></p>
     </div>
-    <div className='home__container'>
-                        { /* HOME CONTAINER 1 */}
-    <div className='home-container1'>
-
-    <div className='picture1'>
-    <img className='home__image1' src="../images/pic1.jpg" alt="pic1"/>
-    <p>YOUR  ONE  STOP  SHOP  FOR    <br></br>
-    ALL  THINGS  <span>"BOTANICAL"</span> <br></br>
-      AND  <span>"DECOR"</span></p>
-    <button className='getStated_btn'>Get Started</button>
-    </div>
-    <div className='picture2'>
-    <img className='home__image2' src='../images/radison.jpg' alt="pic2"/>
-    <img className='home__image3' src='../images/heritage.jpg' alt="pic3"/>
-      </div>
     
-      </div>
-                    {/* HOME CONTAINER 2  */}
-        <div className='home-container2'>
-          <div className='content-box1'>
-            <h2  > <FcShipped /> Delivery within 1-2 days</h2>
-          <p>Guranteed delivery of product and <br></br> services within specified timestamp with <br></br>proper management.</p>
-          </div>
-          <div className='content-box2'>
-            <h2> <FcServices /> Top-notch Service</h2>
-          <p>Servicing more than 100+ clients with <br></br> our product and assistance with <br></br>satisfaction and bliss.</p>
-          </div>
-          <div className='content-box3'>
-          <h2> <FcShop /> Office Hours 8 A.M-5 P.M</h2>
-          <p>Open at only office hours and closed<br></br> on weekends. </p>
-          </div>
-        </div>
-      
+    <div className="flex flex-col object-contain w-[30%]">
+    <div className="w-full h-[300px] md:h-[50%]">
+    <img src="./images/heritage.jpg" className="object-cover w-full h-full " alt="pic1"/>
+    </div>
 
-                          {/* HOME CONTAINER 3 */}
-<div className='home-container3'>
+    <div className="w-full h-[300px] md:h-[50%]">
+    <img src="./images/radison.jpg" className="object-cover w-full h-full " alt="pic1"/>
+    </div>
+    </div>
+    {/* main body div sec1 ends */}
+</div>
+
+<div className="section2 mt-7 w-full h-auto md:h-auto">
+  <div className="flex justify-evenly items-center mx-1 p-2 md:h-auto">
+    <div className="service1 w-[110px] h-auto md:h-auto bg-slate-300 rounded-xl text-black font-writingFont md:w-[300px] text-center">
+      <h1 className="p-3 mt-2 font-sans font-bold text-4xl md:text-6xl">
+        <CountUp end={25}
+        duration={6}></CountUp>
+        +
+      </h1>
+      <p className="mx-5 my-2 p-1 font-sans text-sm text-start md:text-xl ">Years of Service</p>
+    </div>
+    <div className="service2  w-[110px] h-auto md:h-auto bg-slate-300 rounded-xl text-black font-writingFont md:w-[300px] text-center ">
+    <h1 className="p-3 mt-2 font-sans font-bold text-4xl md:text-6xl">
+    <CountUp end={30}
+        duration={6}></CountUp>
+        +
+      </h1>
+      <p className=" mx-5 my-2 p-1 font-sans text-sm text-start md:text-xl">Years of Experience</p>
+    </div>
+    <div className="service3  w-[110px] h-auto md:h-auto bg-slate-300 rounded-xl md:w-[300px] text-center ">
+    <h1 className="p-3 mt-2 font-sans font-bold text-4xl md:text-6xl">
+    <CountUp end={40}
+        duration={6}></CountUp>
+        +
+      </h1>
+      <p className=" mx-5 my-2 p-1 font-sans text-sm text-start md:text-xl">Years of Service</p>
+    </div>
+  </div>
+
+</div>
+
+    {/* main body div sec3 starts */}
+<div className="Section3 mt-7  w-full h-auto md:h-auto bg-green-400flex justify-evenly items-center bg-red-800">
+  <h1 className="text-black font-LogoFont text-center text-3xl md:text-5xl"> Our Services</h1>
+    <Carousel responsive={responsive} className="flex justify-evenly items-center ">
+    <div className="products  w-[150px] h-[100px] md:h-auto m-4 bg-green-700 rounded-xl">
+  <img src='' className='' alt='Product'/>
+  </div>
+
+  <div className="Decor">
+  <img src='' className=' w-[150px] h-[100px] md:h-auto m-4 bg-green-700 rounded-xl' alt='Decor'/>
+
+  </div>
+  <div className="gardening">
+  <img src='' className=' w-[150px] h-[100px] md:h-auto m-4 bg-green-700 rounded-xl' alt='Gardening  '/>
+  </div>
+
+  <div className="Ceramics">
+  <img src='' className=' w-[150px] h-[100px] md:h-auto m-4 bg-green-700 rounded-xl' alt='Ceramics'/>
+  </div>
+    </Carousel>
   
-<div className='services-container'>
-
-<h1 className='serviceh1'> OUR SERVICES</h1>
-
-
-<Carousel responsive={responsive}  className="carousel-inner">
-
-  <div className='service-content1'>
-      <img className="imageservice1" src="../images/productt.jpg " alt="Products"/>
-      <button>Products</button>
-        </div>
-
-  <div className='service-content2'>
-      <img className="imageservice2" src="../images/heritage.jpg " alt="Decorations"/>
-      <button>Decorations</button>
-  </div>
-
-  <div className='service-content3'>
-  <img className="imageservice3" src="../images/gardening.jpg" alt="Gardening"/>
-  <button>Gardening</button>
-  </div>
-
-  <div className='service-content4'>
-  <img className="imageservice4" src="../images/rentalservice.jpg " alt="Rentals"/>
-  <button>Rentals</button>
-  </div>
-  </Carousel>
-
-</div>
 </div>
 
 
-                        {/* HOME CONTAINER 4 */}
-<div className='home-container4'>
+{/* main-body-div */}
+            </div>
+          <div className="footer">
 
+          </div>
 
-<div className='Card-container'>
+            {/* appDiv */}
+    </div>
+    );
+};
 
-<h1 className='Cardh1'> SHOP WITH US <RiShoppingBag4Fill/> </h1>
-
-
-<Carousel responsive={responsive}  className="carousel-inner">
-
-<div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-
-        <div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-
-        <div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-
-        <div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-
-        <div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-
-        <div className='product-Card1'>
-      <img className="Cardimg1" src="../images/productt.jpg " alt="card1"/>
-      <h2 className='Card-title'>Monstera Plant</h2>
-      <h3 className='Card-price'>NPR-2200/-</h3>
-      <button>Add to Cart</button>
-        </div>
-  </Carousel>
-
-</div>
-</div>          {/* HOME CONTAINER 4 ENDS */}
-
-                  {/* HOME CONTAINER 5 TESTIMONIALS */}
-
-      <div className="home-container5">
-
-        
-      </div>
-</div>     {/* //HOME CONTAINER END */}
-
-
-</div>    // MAIN HOME DIV END
-
-);
-}
 export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+    extend: {
+        fontFamily: {
+        LogoFont: "'Tai Heritage Pro', serif;",
+        writingFont:"'Lato', sans-serif;"
+        },
+        keyframes:{
+            'word-rotate':{
+                '0%,100%':{opacity:'0'},
+                '50':{opacity:'1'},
+            },
+        },
+        animation: {
+        'bounce-slow': 'bounce 2s linear infinite',
+        'word-rotate':'blinking-cursor 0.8s cubic-bezier(0.68, 0.01, 0.01, 0.99) infinite'
+        },
+        
+    },
+    },
+    plugins: [],
+}
