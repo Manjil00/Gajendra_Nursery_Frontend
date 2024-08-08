78a368620e38addd7ba13ba06a30485e633665e3
ACER <210220@softwarica.edu.np>
2024-08-08 23:22:52 +0545

    products and decor page f-end

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+
+// Components
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import './index.css';
+
+// ICONS
+import { CiSearch } from "react-icons/ci";
+
 
 const Product = () => {
   return (
@@ -12,7 +18,7 @@ const Product = () => {
   <div className="body flex w-full h-[100%]  relative z-[-1]">
 
     <div className="sidebar h-[100%] w-[20%] md:w-[15%] bg-green-700">
-      <ul className='flex flex-col justify-evenly items-center my-10 gap-5'>
+      <ul className='flex flex-col justify-evenly items-center my-10 gap-5 mb-[200px] px-5 '>
 
         <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline font-semibold'>Products</li>
         <li className='text-sm md:text-xl text-appleblack font-writingFont '>Indoor</li>
@@ -33,18 +39,74 @@ const Product = () => {
         
         <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline'>Gardening</li>
         <li className='text-sm md:text-xl text-appleblack font-writingFont '>Rock Garden</li>
-        <li className='text-sm md:text-xl text-appleblack font-writingFont '>Garden Maintainence</li>
-
-
-
+        <li className='text-sm md:text-xl text-appleblack font-writingFont '>Maintainence</li>
       </ul>
 
-
-
-      
     </div>
 
-    <div className="section2">
+    <div className="section2 w-[80%] h-full gap-10">
+
+      <div className="searchbar h-[30px] md:h-[50px] w-[70%] bg-green-700  rounded-md mx-auto my-8 
+      hover:cursor-text  flex items-center justify-between px-5">
+
+        <input className='bg-transparent text-sm md:text-xl ' placeholder='Search for Products'></input>
+        <CiSearch className='h-[20px] w-[20px] md:h-[30px] md:w-[30px]'/>
+      </div>
+
+      <div className="cardPlace grid grid-cols-2 md:grid-cols-3 h-auto w-full md:mx-10 gap-5 md:gap-10 p-3">
+        
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/luckybamboo.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/snakeplant.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/succulent.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+
+
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/succulent.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/snakeplant.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+      <div className="card bg-black w-[120px] h-[160px] md:h-[400px] md:w-[300px] rounded-xl ml-3 drop-shadow-2xl">
+        <img src='./images/luckybamboo.jpg' className=' rounded-xl h-[90px] md:h-[250px] mx-auto w-[90%] mt-2' alt='Card1'></img>
+        <h1 className=''>Lucky Bamboo</h1>
+
+        {/* INCR AND DECR */}
+        <button className='button-default'>Add to Cart</button>
+      </div>
+
+        
+
+      </div>
+      
+
 
     </div>
 
