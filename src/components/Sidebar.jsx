c82295920632347366b88c635b71562d5d8ca25b
ACER <210220@softwarica.edu.np>
2024-12-11 22:01:22 +0545

    color hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,29 +2,29 @@ import React from 'react';
 
 const Sidebar = () => {
 return (
-    <div className="sidebar h-[100%] w-[20%] md:w-[15%] bg-green-700 cursor-pointer">
-    <ul className='flex flex-col justify-evenly items-center my-10 gap-5 mb-[200px] px-5 '>
+    <div className="sidebar h-[100%] w-[20%] md:w-[15%] cursor-pointer">
+    <ul className='flex flex-col justify-evenly items-center my-10 gap-5 mb-[200px] px-5 text-appleblack font-writingFont'>
 
-    <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline font-semibold hover:text-xl hover:md:text-5xl'>Products</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Indoor</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Outdoor</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Seasonal</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Others</li>
+    <li className='text-md md:text-3xl text-slate-500 font-LogoFont font-semibold'>Products</li>
+    <li className='text-sm md:text-xl   hover:text-md hover:md:text-2xl'>Indoor</li>
+    <li className='text-sm md:text-xl   hover:text-md hover:md:text-2xl'>Outdoor</li>
+    <li className='text-sm md:text-xl   hover:text-md hover:md:text-2xl'>Seasonal</li>
+    <li className='text-sm md:text-xl   hover:text-md hover:md:text-2xl'>Others</li>
 
-    <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline hover:text-xl hover:md:text-5xl'>Decorations</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Stage</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Backdrop</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Mandap</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Entrance</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Photo-Booth</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Name-Board</li>
+    <li className='text-md md:text-3xl text-slate-500 font-LogoFont'>Decorations</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Stage</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Backdrop</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Mandap</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Entrance</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Photo-Booth</li>
+    <li className='text-sm md:text-xl  hover:text-md hover:md:text-2xl'>Name-Board</li>
 
-    <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline hover:text-xl hover:md:text-5xl'>Ceramics</li>
-    <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline hover:text-xl hover:md:text-5xl'>Fertilizers</li>
+    <li className='text-md md:text-3xl text-slate-500 font-LogoFont'>Ceramics</li>
+    <li className='text-md md:text-3xl text-slate-500 font-LogoFont'>Fertilizers</li>
     
-    <li className='text-md md:text-3xl text-bgcolor font-LogoFont underline hover:text-xl hover:md:text-5xl'>Gardening</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Rock Garden</li>
-    <li className='text-sm md:text-xl text-appleblack font-writingFont hover:text-md hover:md:text-2xl'>Maintainence</li>
+    <li className='text-md md:text-3xl text-slate-500 font-LogoFont'>Gardening</li>
+    <li className='text-sm md:text-xl hover:text-md hover:md:text-2xl'>Rock Garden</li>
+    <li className='text-sm md:text-xl hover:text-md hover:md:text-2xl'>Maintainence</li>
     </ul>
 
 </div>
